092ed67583f6fd7978562451a5dc6193cd8b9240
Gilean Alducin <gileanmk@gmail.com>
2025-02-09 22:34:56 -0600

    Tarea 1

---
+++ tipos-de-dato.js
@@ -0,0 +1,13 @@
+// ~Gilean Alducin~
+
+console.log(typeof 42);
+console.log(typeof 'Veinticinco');
+console.log(typeof -666);
+console.log(typeof true);
+console.log(typeof 0);
+console.log(typeof ""); //Unexpected token
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof false);
+console.log(typeof 1);
+console.log(typeof "if");
